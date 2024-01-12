9561c8d54d5bde3ad5378cc8878125677c8684ed
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-12 10:54:19 +0000

    Removed slash from site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://philmanwithbeard.github.io/",
+  site: "https://philmanwithbeard.github.io",
   base: "/weAreMany",
 });
