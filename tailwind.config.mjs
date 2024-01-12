afcca7b57653c6a5a4f86e416621960af25093be
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-12 14:16:45 +0000

    custom colours

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#ffffff",
+        yellow: "#F0D898",
+        "sky-blue": "#CBDCE3",
+        red: "#C06A45",
+        "dark-blue": "#5B6D93",
+        green: "#8C8A7B",
+        black: "#545454",
+      },
+    },
+  },
+  plugins: [],
+};
