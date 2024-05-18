923dc76eac7065e47c5d507adc74ce28b744dacc
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-05-18 14:13:43 +0100

    Creating a branch with SSR (output: 'server') enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://philmanwithbeard.github.io",
   base: "/weAreMany",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "hybrid",
+  adapter: vercel(),
 });
