0a57d7e6a86821d70e72ed0a943efeff80df1df7
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-06-05 20:59:58 +0100

    install astrodb

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://philmanwithbeard.github.io",
   base: "/weAreMany",
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   output: "hybrid",
   adapter: vercel(),
 });
